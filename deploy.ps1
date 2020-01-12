2529ac5dc152b06231b148840669c1c7ce27b46e
aaronamm <theeranitp@gmail.com>
2020-01-12 16:18:19 +0700

    Reuse environment variables

--- deploy.ps1
+++ deploy.ps1
@@ -9,40 +9,26 @@ $VerbosePreference = "Stop"
 
 Get-Command node
 $ARTIFACTS = "$PSScriptRoot\..\artifacts"
-if (-not(Test-Path -Path $ARTIFACTS)) {
-    New-Item -ItemType Directory -Path $ARTIFACTS -Force
-}
 
 # Set deployment source folder
-if (-not $env:SCM_REPOSITORY_PATH) {
-    'Set $DEPLOYMENT_SOURCE variable from $env:SCM_REPOSITORY_PATH'
-	$DEPLOYMENT_SOURCE = $env:SCM_REPOSITORY_PATH
-}
-
-if (-not(Get-Variable -Name DEPLOYMENT_SOURCE -ErrorAction Ignore)) {
+if (-not $env:DEPLOYMENT_SOURCE) {
     'Set $DEPLOYMENT_SOURCE variable from current directory'
-	$DEPLOYMENT_SOURCE = $PSScriptRoot
+	$env:DEPLOYMENT_SOURCE = $PSScriptRoot
 }
 
 if (-not $env:DEPLOYMENT_TARGET) {
     'Set $DEPLOYMENT_TARGET variable'
-	$DEPLOYMENT_TARGET = "$ARTIFACTS\wwwroot"
-}else{
-	$DEPLOYMENT_TARGET = $env:DEPLOYMENT_TARGET
+	$env:DEPLOYMENT_TARGET = "$ARTIFACTS\wwwroot"
 }
 
 if (-not $env:NEXT_MANIFEST_PATH) {
     'Set $NEXT_MANIFEST_PATH variable'
-	$NEXT_MANIFEST_PATH = "$ARTIFACTS\manifest"
-}else{
-	$NEXT_MANIFEST_PATH = $env:NEXT_MANIFEST_PATH
+	$env:NEXT_MANIFEST_PATH = "$ARTIFACTS\manifest"
 }
 
 if (-not $env:PREVIOUS_MANIFEST_PATH) {
 	'Set $PREVIOUS_MANIFEST_PATH variable'
-	$PREVIOUS_MANIFEST_PATH = "$ARTIFACTS\manifest"
-}else{
-	$PREVIOUS_MANIFEST_PATH = $env:PREVIOUS_MANIFEST_PATH
+	$env:PREVIOUS_MANIFEST_PATH = "$ARTIFACTS\manifest"
 }
 
 if (-not $env:KUDU_SYNC_CMD) {
@@ -51,31 +37,28 @@ if (-not $env:KUDU_SYNC_CMD) {
 
 	# Locally just running "kuduSync" would also work
     'Set $KUDU_SYNC_CMD varialble'
-	$KUDU_SYNC_CMD = "$env:APPDATA\npm\kuduSync.cmd"
-}else{
-	$KUDU_SYNC_CMD = $env:KUDU_SYNC_CMD
+	$env:KUDU_SYNC_CMD = "$env:APPDATA\npm\kuduSync.cmd"
 }
 
 if (-not $env:DEPLOYMENT_TEMP) {
 	$random = Get-Random -InputObject $(0..$([int16]::MaxValue))
 
     'Set $DEPLOYMENT_TEMP and $CLEAN_LOCAL_DEPLOYMENT_TEMP variables'
-	$DEPLOYMENT_TEMP = "$env:TEMP\___deployTemp$random"
+	$env:DEPLOYMENT_TEMP = "$env:TEMP\___deployTemp$random"
 	$CLEAN_LOCAL_DEPLOYMENT_TEMP = $true
 }else{
-	$DEPLOYMENT_TEMP = $env:DEPLOYMENT_TEMP
 	$CLEAN_LOCAL_DEPLOYMENT_TEMP = $false
 }
 
 if ($CLEAN_LOCAL_DEPLOYMENT_TEMP) {
-    'About to remove and create new $DEPLOYMENT_TEMP directory'
-    if (Test-Path -Path $DEPLOYMENT_TEMP) {
-        'Remove $DEPLOYMENT_TEMP directory'
-        Remove-Item -Path $DEPLOYMENT_TEMP -Force -Recurse
+    'About to remove and create new $env:DEPLOYMENT_TEMP directory'
+    if (Test-Path -Path $env:DEPLOYMENT_TEMP) {
+        'Remove $env:DEPLOYMENT_TEMP directory'
+        Remove-Item -Path $env:DEPLOYMENT_TEMP -Force -Recurse
     }
 
-    'New $DEPLOYMENT_TEMP directory'
-    New-Item -ItemType Directory -Path $DEPLOYMENT_TEMP
+    'New $env:DEPLOYMENT_TEMP directory'
+    New-Item -ItemType Directory -Path $env:DEPLOYMENT_TEMP
 }
 
 # Define default node version in WEBSITE_NODE_DEFAULT_VERSION App Setting
@@ -90,19 +73,20 @@ $MSBUILD_PATH = "${env:ProgramFiles(x86)}\MSBuild-15.3.409.57025\MSBuild\15.0\Bi
 'Get MSBuild version'
 & "$MSBUILD_PATH" -version
 
-$SOLUTION_PATH = "$DEPLOYMENT_SOURCE\Codesanook.ReactJS.sln"
-$PROJECT_PATH = "$DEPLOYMENT_SOURCE\Codesanook.ReactJS.ServerSideRendering\Codesanook.ReactJS.ServerSideRendering.csproj"
-$PROJECT_DIR  = "$DEPLOYMENT_SOURCE\Codesanook.ReactJS.ServerSideRendering"
+$SOLUTION_PATH = "$env:DEPLOYMENT_SOURCE\Codesanook.ReactJS.sln"
+$PROJECT_PATH = "$env:DEPLOYMENT_SOURCE\Codesanook.ReactJS.ServerSideRendering\Codesanook.ReactJS.ServerSideRendering.csproj"
+$PROJECT_DIR  = "$env:DEPLOYMENT_SOURCE\Codesanook.ReactJS.ServerSideRendering"
 
 "-----------------Variables---------------------------------"
-"ARTIFACTS = $ARTIFACTS"
-"DEPLOYMENT_SOURCE = $DEPLOYMENT_SOURCE"
-"DEPLOYMENT_TARGET = $DEPLOYMENT_TARGET"
-"NEXT_MANIFEST_PATH = $NEXT_MANIFEST_PATH"
-"PREVIOUS_MANIFEST_PATH = $PREVIOUS_MANIFEST_PATH"
+"ARTIFACTS = $env:ARTIFACTS"
+"DEPLOYMENT_SOURCE = $env:DEPLOYMENT_SOURCE"
+"DEPLOYMENT_TARGET = $env:DEPLOYMENT_TARGET"
+"NEXT_MANIFEST_PATH = $env:NEXT_MANIFEST_PATH"
+"PREVIOUS_MANIFEST_PATH = $env:PREVIOUS_MANIFEST_PATH"
+"KUDU_SYNC_CMD = $env:KUDU_SYNC_CMD"
+"DEPLOYMENT_TEMP = $env:DEPLOYMENT_TEMP"
+"IN_PLACE_DEPLOYMENT = $env:IN_PLACE_DEPLOYMENT"
 
-"KUDU_SYNC_CMD = $KUDU_SYNC_CMD"
-"DEPLOYMENT_TEMP = $DEPLOYMENT_TEMP"
 "CLEAN_LOCAL_DEPLOYMENT_TEMP = $CLEAN_LOCAL_DEPLOYMENT_TEMP"
 "MSBUILD_PATH = $MSBUILD_PATH"
 
@@ -111,7 +95,6 @@ $PROJECT_DIR  = "$DEPLOYMENT_SOURCE\Codesanook.ReactJS.ServerSideRendering"
 "PROJECT_DIR = $PROJECT_DIR" 
 
  "-----------------Variables END ---------------------------------"
-
 
 "Handling .NET Web Application deployment."
 
@@ -151,10 +134,10 @@ if(Test-Path -Path "$PROJECT_DIR\package.json") {
 nuget restore "$SOLUTION_PATH"
 
 "Build .NET project to the temp directory"
-"$DEPLOYMENT_SOURCE\\"
+"$env:DEPLOYMENT_SOURCE\\"
 
 if(-not $env:IN_PLACE_DEPLOYMENT){
-	"Building with MSBUILD to '$DEPLOYMENT_TEMP'" 
+	"Building with MSBUILD to '$env:DEPLOYMENT_TEMP'" 
 
 	& "$MSBUILD_PATH" `
         "$PROJECT_PATH" `
@@ -162,28 +145,32 @@ if(-not $env:IN_PLACE_DEPLOYMENT){
         /verbosity:minimal `
         /t:Build `
         /t:pipelinePreDeployCopyAllFilesToOneFolder `
-        /p:_PackageTempDir="$DEPLOYMENT_TEMP" `
+        /p:_PackageTempDir="$env:DEPLOYMENT_TEMP" `
         /p:AutoParameterizationWebConfigConnectionStrings=false `
         /p:Configuration=Release `
         /p:UseSharedCompilation=false `
-        /p:SolutionDir="$DEPLOYMENT_SOURCE\\" `
+        /p:SolutionDir="$env:DEPLOYMENT_SOURCE\\" `
         $env:SCM_BUILD_ARGS
         # Set SCM_BUILD_ARGS apps settings to whatever string you want to append to the msbuild command line.
+
+    if (-not $?) {
+        throw "Error building .NET project"
+    }
 } 
 
 "Output structure of build result"
-tree "$DEPLOYMENT_TEMP" /f /a 
+tree "$env:DEPLOYMENT_TEMP" /f /a 
 # /f Displays the names of the files in each directory.
 # /a Specifies that tree is to use text characters instead of graphic characters to show the lines that link subdirectories.
 
 if(-not $env:IN_PLACE_DEPLOYMENT){
 	"Kudu syncing" 
-	& "$KUDU_SYNC_CMD" `
+	& "$env:KUDU_SYNC_CMD" `
         -v 50 `
-        -f "$DEPLOYMENT_TEMP" `
-        -t "$DEPLOYMENT_TARGET" `
-        -n "$NEXT_MANIFEST_PATH" `
-        -p "$PREVIOUS_MANIFEST_PATH" `
+        -f "$env:DEPLOYMENT_TEMP" `
+        -t "$env:DEPLOYMENT_TARGET" `
+        -n "$env:NEXT_MANIFEST_PATH" `
+        -p "$env:PREVIOUS_MANIFEST_PATH" `
         -i ".git;.hg;.deployment;deploy.cmd;deploy.ps1;node_modules;"
 
     if (-not $?) {
