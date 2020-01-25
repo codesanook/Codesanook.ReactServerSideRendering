0397b1e40666613b8ea8241fedb34953207b07df
aaronamm <theeranitp@gmail.com>
2020-01-25 14:21:22 +0700

    Use PowerShell module for DeploymentUtility

--- deploy.ps1
+++ deploy.ps1
@@ -1,39 +1,18 @@
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/set-strictmode?view=powershell-7
 Set-StrictMode -Version Latest
 
-"CUSTOM_VARIABLE: $Env:CUSTOM_VARIABLE"
-"SCM_REPOSITORY_PATH: $Env:SCM_REPOSITORY_PATH"
-
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7#erroractionpreference
-"Current `$ErrorActionPreference value: $ErrorActionPreference"
+$ErrorActionPreference = "Stop"
 
-function Invoke-ExternalCommand {
-    param (
-        [scriptblock] $ScriptBlock
-    )
-    # Displays an error message and continue executing if there is standard error.
-    $ErrorActionPreference = "Continue"
-    & $ScriptBlock 2>&1 
-    if ($LastExitCode) {
-        "Failed exitCode=$LastExitCode, command=$($ScriptBlock.ToString())"
-    }
-}
+Import-Module -Name .\DeploymentUtility -Force
 
-function Exit-ScriptIfError {
-    if ($LastExitCode) {
-        "Command failed with exitCode=$LastExitCode"
-        Exit 1 
-    }
-}
-
-# Verify node.js installed
+"Verify if Node.js installed"
 if (-not (Get-Command -Name node -ErrorAction Ignore)) {
     throw "Missing node.js executable, please install node.js." +
     "If already installed, make sure it can be reached from current environment."
 }
 
 $ARTIFACTS = "$PSScriptRoot\..\artifacts"
-
 # Set deployment source folder
 if (-not $Env:DEPLOYMENT_SOURCE) {
     'Set $DEPLOYMENT_SOURCE variable from current directory'
@@ -86,42 +65,35 @@ if ($CLEAN_LOCAL_DEPLOYMENT_TEMP) {
     New-Item -ItemType Directory -Path $Env:DEPLOYMENT_TEMP
 }
 
+$MSBUILD_PATH = "${env:ProgramFiles(x86)}\MSBuild-15.3.409.57025\MSBuild\15.0\Bin\MSBuild.exe"
+"`$MSBUILD_PATH is set to $MSBUILD_PATH"
+
+# Log environment variables
+$environmentNameToWriteValue = @(
+    "DEPLOYMENT_SOURCE"
+    "DEPLOYMENT_TARGET"
+    "NEXT_MANIFEST_PATH"
+    "PREVIOUS_MANIFEST_PATH"
+    "KUDU_SYNC_CMD"
+    "DEPLOYMENT_TEMP"
+    "IN_PLACE_DEPLOYMENT"
+    "WEBSITE_NODE_DEFAULT_VERSION"
+    "SCM_REPOSITORY_PATH"
+    "Path" 
+    "SOLUTION_PATH"
+    "PROJECT_DIR"
+    "PROJECT_PATH"
+    "CUSTOM_VARIABLE" # Defined in .dployment
+)
+Write-EnviromentValue -EnvironmentName $environmentNameToWriteValue
+
 # Define default node version in WEBSITE_NODE_DEFAULT_VERSION App Setting
 # Find all Node.js versions from api/diagnostics/runtime
 # https://codesanook-reactjs-server-side-rendering.scm.azurewebsites.net/api/diagnostics/runtime
-"Default node version $Env:WEBSITE_NODE_DEFAULT_VERSION"
-
-# Set MSBUILD_PATH
-'Set MSBUILD_PATH'
-$MSBUILD_PATH = "${env:ProgramFiles(x86)}\MSBuild-15.3.409.57025\MSBuild\15.0\Bin\MSBuild.exe"
-
-$SOLUTION_PATH = "$Env:DEPLOYMENT_SOURCE\Codesanook.ReactJS.sln"
-$PROJECT_PATH = "$Env:DEPLOYMENT_SOURCE\Codesanook.ReactJS.ServerSideRendering\Codesanook.ReactJS.ServerSideRendering.csproj"
-$PROJECT_DIR = "$Env:DEPLOYMENT_SOURCE\Codesanook.ReactJS.ServerSideRendering"
-
-"-----------------Variables---------------------------------"
-"ARTIFACTS = $Env:ARTIFACTS"
-"DEPLOYMENT_SOURCE = $Env:DEPLOYMENT_SOURCE"
-"DEPLOYMENT_TARGET = $Env:DEPLOYMENT_TARGET"
-"NEXT_MANIFEST_PATH = $Env:NEXT_MANIFEST_PATH"
-"PREVIOUS_MANIFEST_PATH = $Env:PREVIOUS_MANIFEST_PATH"
-"KUDU_SYNC_CMD = $Env:KUDU_SYNC_CMD"
-"DEPLOYMENT_TEMP = $Env:DEPLOYMENT_TEMP"
-"IN_PLACE_DEPLOYMENT = $Env:IN_PLACE_DEPLOYMENT"
-
-"CLEAN_LOCAL_DEPLOYMENT_TEMP = $CLEAN_LOCAL_DEPLOYMENT_TEMP"
-"MSBUILD_PATH = $MSBUILD_PATH"
-
-"SOLUTION_PATH = $SOLUTION_PATH" 
-"PROJECT_PATH = $PROJECT_PATH" 
-"PROJECT_DIR = $PROJECT_DIR" 
-"-----------------Variables END ---------------------------------"
-
-"Current node and npm version"
 "node version $(& node --version)"
 "npm version $(& npm --version)"
 
-"Verify yarn installed" 
+"Verify if yarn installed" 
 $Env:Path += ";$Env:AppData\npm"
 if (Get-Command -Name yarn -ErrorAction Ignore) {
     "Update yarn as a global tool to the latest version"
@@ -134,12 +106,13 @@ else {
     Exit-ScriptIfError
 }
 
-if (-not (Test-Path -Path "$PROJECT_DIR\package.json")) {
-    throw "There is no $PROJECT_DIR\package.json file"
+"Bulding Node project"
+if (-not (Test-Path -Path "$Env:PROJECT_DIR\package.json")) {
+    throw "There is no $Env:PROJECT_DIR\package.json file"
 }
 
 # Install node packages
-Push-Location -Path  $PROJECT_DIR
+Push-Location -Path  $Env:PROJECT_DIR
 "Installing node packages with yarn"
 Invoke-ExternalCommand -ScriptBlock { & yarn install --silent }
 Exit-ScriptIfError
@@ -152,15 +125,15 @@ Pop-Location
 
 "Handling .NET Web Application deployment."
 "Restore NuGet packages"
-Invoke-ExternalCommand -ScriptBlock { & nuget restore "$SOLUTION_PATH" }
+Invoke-ExternalCommand -ScriptBlock { & nuget restore "$Env:SOLUTION_PATH" }
 Exit-ScriptIfError
 
 "Build .NET project to the temp directory"
 if (-not $Env:IN_PLACE_DEPLOYMENT) {
     "Building with MSBUILD to '$Env:DEPLOYMENT_TEMP'" 
     Invoke-ExternalCommand -ScriptBlock { 
-        & "$MSBUILD_PATH" `
-            "$PROJECT_PATH" `
+        & "$Env:MSBUILD_PATH" `
+            "$Env:PROJECT_PATH" `
             /nologo `
             /verbosity:minimal `
             /t:Build `
@@ -171,7 +144,7 @@ if (-not $Env:IN_PLACE_DEPLOYMENT) {
             /p:UseSharedCompilation=false `
             /p:SolutionDir="$Env:DEPLOYMENT_SOURCE" `
             $Env:SCM_BUILD_ARGS
-        # Set SCM_BUILD_ARGS apps settings to whatever string you want to append to the msbuild command line.
+        # Set SCM_BUILD_ARGS App Services Apps Settings to string you want to append to the msbuild command line.
     }
     Exit-ScriptIfError
 }
