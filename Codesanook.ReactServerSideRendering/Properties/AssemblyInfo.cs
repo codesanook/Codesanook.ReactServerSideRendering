4a25c50c9f6313dbd3c635bd816d31156a40c602
aaronamm <theeranitp@gmail.com>
2020-08-29 16:57:13 +0700

    Rename the project and update React.Core, JavaScript engine to the latest version

--- Codesanook.ReactServerSideRendering/Properties/AssemblyInfo.cs
+++ Codesanook.ReactServerSideRendering/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Codesanook.ReactJS.ServerSideRendering")]
+[assembly: AssemblyTitle("Codesanook.ReactServerSideRendering")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Codesanook.ReactJS.ServerSideRendering")]
+[assembly: AssemblyProduct("Codesanook.ReactServerSideRendering")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
